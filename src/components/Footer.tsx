75cc551987c1261569cc4d3a2a56bf71f91a0137
Joshua Marcille <joshua.marcille@gmail.com>
2025-11-27 03:19:43 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,24 @@
 import { useQRScoutState } from '@/store/store';
 import { Heart } from 'lucide-react';
-import { Logo } from './Logo';
+import Logo4909 from '@/assets/4909_Logo_white_on_transparent.svg';
 
 export function Footer() {
   const teamNumber = useQRScoutState(state => state.formData.teamNumber);
+
   return (
     <footer>
       <div className="mt-8 flex flex-col items-center justify-center p-2 gap-2">
-        <div className="h-24 w-96">
-          <Logo />
+
+        {/* Logo */}
+        <div className="h-24 w-96 flex items-center justify-center">
+          <img
+            src={Logo4909}
+            alt="Team 4909 Logo"
+            className="h-full w-full object-contain"
+          />
         </div>
+
+        {/* Heart + Team number */}
         {teamNumber !== 2713 && (
           <>
             <Heart className="text-primary size-8 fill-primary" />
